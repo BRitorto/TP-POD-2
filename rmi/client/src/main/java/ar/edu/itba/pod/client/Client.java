eaef8b0fab05bb2dbff396d332ee481c30431537
Esteban Kramer <estebankramer@gmail.com>
2019-10-19 19:25:02 -0300

    CLient

--- rmi/client/src/main/java/ar/edu/itba/pod/client/Client.java
+++ rmi/client/src/main/java/ar/edu/itba/pod/client/Client.java
@@ -1,12 +1,85 @@
 package ar.edu.itba.pod.client;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.client.config.ClientNetworkConfig;
+import com.hazelcast.config.GroupConfig;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IMap;
+import org.apache.commons.cli.*;
 
 public class Client {
-    private static Logger logger = LoggerFactory.getLogger(Client.class);
+    private static final String QUERY_OPTION_NAME = "query";
+    private static final String QUERY_OPTION_DESCRIPTION = "todo";
 
-    public static void main(String[] args) {
-        logger.info("rmi Client Starting ...");
+    private static final String ADDRESSES_NAME = "addresses";
+    private static final String ADDRESSES_DESCRIPTION = "todo";
+
+    private static final String IN_PATH_NAME = "inPath";
+    private static final String IN_PATH_DESCRIPTION = "todo";
+
+    private static final String OUT_PATH_NAME = "outPath";
+    private static final String OUT_PATH_DESCRIPTION = "todo";
+
+    private static final String N_NAME = "n";
+    private static final String N_DESCRIPTION = "todo";
+
+    private static final String OACI_NAME = "oaci";
+    private static final String OACI_DESCRIPTION = "todo";
+
+    private static final String MIN_NAME = "min";
+    private static final String MIN_DESCRIPTION = "todo";
+//    TODO add descriptions
+
+
+    public static void main( String[] args ) {
+        Options options;
+        options = new Options();
+        addOption(options, QUERY_OPTION_NAME, QUERY_OPTION_DESCRIPTION, true, true);
+        addOption(options, ADDRESSES_NAME, ADDRESSES_DESCRIPTION, true, true);
+        addOption(options, IN_PATH_NAME, IN_PATH_DESCRIPTION, true, true);
+        addOption(options, OUT_PATH_NAME, OUT_PATH_DESCRIPTION, true, true);
+        addOption(options, N_NAME, N_DESCRIPTION, true, false);
+        addOption(options, OACI_NAME, OACI_DESCRIPTION, true, false);
+        addOption(options, MIN_NAME, MIN_DESCRIPTION, true, false);
+
+        CommandLine commandLine = parse(args, options);
+        ClientConfig clientConfig = getConfig(commandLine);
+        HazelcastInstance client = HazelcastClient.newHazelcastClient(clientConfig);
+//     TODO:   ACA VAN EL SWITCH DE LAS QUERIES
+
     }
+
+    private static ClientConfig getConfig(CommandLine commandLine) {
+        ClientConfig clientConfig = new ClientConfig();
+        clientConfig.setGroupConfig(new GroupConfig("g3", "g3"));
+        String addresses = commandLine.getOptionValue("addresses");
+        String addressesList[] = addresses.split(";");
+        ClientNetworkConfig networkConfig = clientConfig.getNetworkConfig();
+        for(String address : addressesList) {
+            networkConfig.addAddress(address);
+        }
+        return clientConfig;
+    }
+
+    public static CommandLine parse(String[] args, Options options){
+        CommandLineParser parser = new DefaultParser();
+//        HelpFormatter formatter = new HelpFormatter();
+
+        try {
+            return parser.parse(options, args);
+        } catch (ParseException e) {
+            System.out.println( "Parsing failed.  Reason: " + e.getMessage());
+//            formatter.printHelp(this.remoteServiceName, options);
+            System.exit(1);
+            return null;
+        }
+    }
+
+    public static void addOption(Options options, final String name, final String description, final boolean hasArguments, final boolean required) {
+        Option op = new Option(name, name, hasArguments, description);
+        op.setRequired(required);
+        options.addOption(op);
+    }
+
 }
